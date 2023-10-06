34d49887243f1c84156749ab2239475dbcf6cd20
haoranzh <haoran7.zhang@gmail.com>
2023-10-05 20:03:23 -0400

    Fix ModuleNotFound issues on GCS

--- src/model-training/package/setup.py
+++ src/model-training/package/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-REQUIRED_PACKAGES = ["wandb==0.15.11", "tensorflow-hub==0.14.0", "transformers", "torch", "python-json-logger"] # need to specify other packages used for model training
+REQUIRED_PACKAGES = ["wandb==0.15.11", "tensorflow-hub==0.14.0", "transformers", "torch", "python-json-logger", "pandas", "tensorflow", "Pillow"] # need to specify other packages used for model training
 
 setup(
     name="instacap-trainer",
