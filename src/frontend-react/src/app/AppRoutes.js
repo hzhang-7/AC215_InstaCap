7bb830633919c55c48b012b88750bbd2b74156b1
Annabel Yim <annabelyim@g.harvard.edu>
2023-12-05 12:23:13 -0500

    adding things from react tutorial, we need to edit approutes.js and content

---
+++ src/frontend-react/src/app/AppRoutes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Switch } from 'react-router-dom';
+import Home from "../components/Home";
+// import Error404 from '../components/Error/404';
+// import Blank from "../components/Blank";
+// import ImageClassification from "../components/ImageClassification";
+// import Audio2Text from "../components/Audio2Text";
+// import Text2Audio from "../components/Text2Audio";
+// import Plots from "../components/Plots";
+// import Todo from "../components/Todo";
+// import Chat from "../components/Chat";
+
+const AppRouter = (props) => {
+
+  console.log("================================== AppRouter ======================================");
+
+  return (
+    <React.Fragment>
+      <Switch>
+      </Switch>
+    </React.Fragment>
+  );
+}
+
+export default AppRouter;
